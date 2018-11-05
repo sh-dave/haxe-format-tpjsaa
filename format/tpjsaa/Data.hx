bd586f3b9ef268ae5dc36a82028ebcada34ed1e5
David Klein <dave@smokinheads.de>
2018-11-06 00:53:18 +0100

    pivot is optional

--- format/tpjsaa/Data.hx
+++ format/tpjsaa/Data.hx
@@ -3,7 +3,7 @@ package format.tpjsaa;
 typedef Frame = {
 	filename: String,
 	frame: { x: Int, y: Int, w: Int, h: Int },
-	pivot: { x: Float, y: Float },
+	?pivot: { x: Float, y: Float },
 	rotated: Bool,
 	sourceSize: { w: Int, h: Int },
 	spriteSourceSize: { x: Int, y: Int, w: Int, h: Int },
